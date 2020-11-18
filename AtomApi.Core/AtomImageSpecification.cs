23b4963cc0dc93ddee3c01559e1746e4cd38c2fe
wattsma@southernwater.co.uk <matthew.watts@southernwater.co.uk>
2020-11-18 19:48:18 +0000

    Removed weather controller and added first Atom file model

---
+++ AtomApi.Core/AtomImageSpecification.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtomApi.Core
+{
+    public class AtomImageSpecification
+    {
+        public string ImageFileName { get; set; }
+        public string Watermark { get; set; }
+        public string ImageFileExtension { get; set; }
+        public string BackgroundColour { get; set; }
+        public float VerticalResolution { get; set; }
+        public float HorizontalResolution { get; set; }
+    }
+}
